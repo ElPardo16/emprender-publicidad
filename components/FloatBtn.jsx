5168b7e903729c1b1367da3cceda43e3ae55c58c
ElPardo16 <andresito105@gmail.com>
2022-12-24 14:12:29 -0500

    Agregada funcionalidad de whatsApp

--- components/FloatBtn.jsx
+++ components/FloatBtn.jsx
@@ -1,6 +1,9 @@
 import {FaWhatsapp} from 'react-icons/fa'
 export default function FloatBtn() {
+  const WA_NUM = "3192037173"
+  const msg = "Buen dia %0aMe interesa cotizar un producto en especifico con ustedes"
+  const wa_url = `https://api.whatsapp.com/send?phone=+57${WA_NUM}&text=${msg}`
   return (
-    <div className="float"><FaWhatsapp size={40}/></div>
+    <div className="float" onClick={_ => void window.open(wa_url, "_blank")}><FaWhatsapp size={40}/></div>
   )
 }
